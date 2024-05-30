c468467fb4e210f9f353c3d99d3c69269b29923c
Luan <franciscodgail@gmail.com>
2024-05-30 16:46:30 +0800

    fade in on scroll added

--- amplify-icp/src/amplify-icp-frontend/header-scroll.js
+++ amplify-icp/src/amplify-icp-frontend/header-scroll.js
@@ -16,3 +16,27 @@ function scrollFunction() {
         navbar.classList.add("navbar-normal");
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.5, /* Adjust this value based on when you want the animation to trigger */
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
