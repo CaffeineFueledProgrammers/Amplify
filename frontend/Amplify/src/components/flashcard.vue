627cd62119e040e51c97e84df3b2cd748c58f834
Luan <franciscodgail@gmail.com>
2024-06-06 00:19:22 +0800

    added flashcard

--- frontend/Amplify/src/components/flashcard.vue
+++ frontend/Amplify/src/components/flashcard.vue
@@ -1,6 +1,108 @@
 <template>
     <v-app>
-        <main>
-            </main>
+        
+        <div id="app">
+            <h1 class=" d-flex justify-center align-center">Generate Flashcards</h1>
+            <v-container>
+                <v-row class="justify-center align-center">
+                    <v-col v-for="(flashcard, index) in flashcards" :key="index" cols="2" md="6">
+                        <div class="flashcard" @mouseover="flipCard(index)" @mouseleave="flipCard(index)">
+                            <div class="card-inner" :class="{ flipped: flashcard.flipped }">
+                                <div class="card-front">
+                                    {{ flashcard.front }}
+                                </div>
+                                <div class="card-back">
+                                    {{ flashcard.back }}
+                                </div>
+                            </div>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
     </v-app>
 </template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            flashcards: [
+                {
+                    front: "ANKI SUPPORT",
+                    back: "  Amplify allows you to export the flashcards into Anki-supported decks so you can integrate it with your Anki workflow.",
+                    flipped: false,
+                },
+                {
+                    front: "INTELLIGENT FLASHCARD GENERATION",
+                    back: " Amplify helps you generate flashcards for you to use based on your notes.",
+                    flipped: false,
+                },
+            ],
+        };
+    },
+    methods: {
+        flipCard(index) {
+            this.flashcards[index].flipped = !this.flashcards[index].flipped;
+        },
+    },
+};
+</script>
+
+<style scoped>
+#app {
+    margin-top: 10%;
+}
+#app h1 {
+    font-size: 5rem;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 800;
+    color: #fff;
+    text-align: center;
+    margin-bottom:10% ;
+}
+.flashcard {
+    width: 400px;
+    height: 300px;
+    perspective: 1000px;
+    margin: 10px auto;
+}
+.card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+}
+.card-inner.flipped {
+    transform: rotateX(180deg);
+}
+.card-front,
+.card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.card-front {
+    background: rgb(69, 80, 135);
+    background: radial-gradient(circle, rgba(69, 80, 135, 1) 6%, rgba(16, 16, 28, 1) 75%);
+    font-family: "Montserrat", sans-serif;
+    font-size: 1rem;
+    font-weight: 600;
+}
+.card-back {
+    background: #1e1e24;
+    transform: rotateX(180deg);
+    font-family: "Montserrat", sans-serif;
+
+    font-size: 1rem;
+    font-weight: 200;
+    font-optical-sizing: auto;
+}
+</style>
