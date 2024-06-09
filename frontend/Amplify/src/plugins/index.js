f3f7623911e772cffe6efe988226189706935c24
chris1320 <chris1320is@protonmail.com>
2024-06-10 00:34:10 +0800

    style(frontend): apply formatting
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- frontend/Amplify/src/plugins/index.js
+++ frontend/Amplify/src/plugins/index.js
@@ -7,10 +7,9 @@
 // Plugins
 import router from "@/router";
 import vuetify from "./vuetify";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export function registerPlugins(app) {
     app.use(vuetify).use(router);
 }
-
