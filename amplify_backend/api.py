761cc4404abe956112e95c5675bf8c208750b9da
chris1320 <chris1320is@protonmail.com>
2024-06-17 17:07:00 +0800

    feat(backend): implementing basic HTTP auth methods
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- amplify_backend/api.py
+++ amplify_backend/api.py
@@ -4,24 +4,108 @@ Amplify - AI-Powered Note-Taking for Students
 This module contains all the routes for the Flask app.
 """
 
-from fastapi import FastAPI
+# pyright: reportCallInDefaultInitializer=false
+
+import random
+
+from fastapi import Body, Depends, FastAPI, HTTPException, Request, status
+from fastapi.security import HTTPBasicCredentials
+
+from amplify_backend.models.user import User
 
 api: FastAPI = FastAPI()
+# User Database (for testing purposes) and
+# In-memory session storage (for testing purposes)
+users: dict[str, User] = {}  # <username, User>
+sessions: dict[int, int] = {}  # <session_id, user_id>
 
 
-@api.get("/users/register")
-def user_register():
+def authenticate_user(credentials: HTTPBasicCredentials):
+    """
+    Authenticates the user based on the provided credentials.
+    """
+
+    user = users.get(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return user
+
+
+def create_session(user_id: int) -> int:
+    """
+    Creates a new session for the user.
+    """
+
+    session_id = len(sessions) + random.randint(0, 1000000)
+    sessions[session_id] = user_id
+    return session_id
+
+
+def get_user_from_session(request: Request):
+    """
+    Gets the user from the session.
+    """
+
+    session_id = request.cookies.get("session_id")
+    if session_id is None or int(session_id) not in sessions:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid session ID",
+        )
+    return sessions[int(session_id)]
+
+
+@api.post("/register")
+def user_register(username: str = Body(), password: str = Body()):
     """
     The endpoint for user registration.
     """
 
-    return {"message": "User register route"}
+    # check if the username is already taken
+    if users.get(username):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="Username already taken"
+        )
+
+    new_user = User(
+        id=len(users) + 1,
+        username=username,
+        password=password,
+    )
+    users[new_user.username] = new_user
+    return {"message": "User registered successfully"}
 
 
-@api.get("/users/login")
-def user_login():
+@api.post("/login")
+def user_login(user: User = Depends(authenticate_user)):
     """
     The endpoint for user login.
     """
 
-    return {"message": "User login route"}
+    session_id = create_session(user.id)
+    return {"message": "Logged in successfully", "session_id": session_id}
+
+
+# TODO: Remove these development endpoints before deployment
+
+
+@api.get("/dev/users")
+def get_users(user_id: int = Depends(get_user_from_session)):
+    """
+    A temporary development endpoint to get all registered users.
+    """
+
+    return (user_id, users)
+
+
+@api.get("/dev/sessions")
+def get_sessions(user_id: int = Depends(get_user_from_session)):
+    """
+    A temporary development endpoint to get all sessions.
+    """
+
+    return (user_id, sessions)
---
+++ amplify_backend/models/user.py
@@ -0,0 +1,20 @@
+"""
+Amplify - AI-Powered Note-Taking for Students
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    """This class represents a user.
+
+    Attributes:
+        id: The unique identifier of the user.
+        username: The chosen username of the user.
+        password: The hashed password of the user.
+    """
+
+    id: int
+    username: str
+    password: str
