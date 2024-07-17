9724a8532fd1f806577a4302a4863289d0223001
chris1320 <chris1320is@protonmail.com>
2024-07-18 01:04:09 +0800

    refactor: remove Python backend
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- amplify_backend/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Amplify - AI-Powered Note-Taking for Students
-"""
-
-from amplify_backend.api import api
-
-# let FastAPI know that this is the main app
-app = api
--- amplify_backend/info.py
+++
@@ -1,9 +0,0 @@
-"""
-Amplify - AI-Powered Note-Taking for Students
-"""
-
-NAME: str = "Amplify"
-VERSION: tuple[int, ...] = (0, 0, 1)
-TITLE: str = f"{NAME} v{'.'.join(map(str, VERSION))}"
-
-DATABASE_URL: str = "sqlite:///./data/amplify.db"
