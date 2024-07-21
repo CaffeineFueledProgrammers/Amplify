2da884e3d35810367d21df00b35c2890f5313eeb
Chris1320 <chris1320is@protonmail.com>
2024-07-22 02:55:07 +0800

    feat: add Firebase config

--- amplify/src/firebasehandler/index.js
+++ amplify/src/firebasehandler/index.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
 
-const config = {};
+const config = {
+    apiKey: "AIzaSyDxW3e9NfW75Oly4d9aFuWhEZmAW6KNS18",
+    authDomain: "amplify-9dde5.firebaseapp.com",
+    projectId: "amplify-9dde5",
+    storageBucket: "amplify-9dde5.appspot.com",
+    messagingSenderId: "844563357822",
+    appId: "1:844563357822:web:ec31cd57172537b6e2f82f",
+    measurementId: "G-GRFB6F8JNF",
+};
 
 export const firebaseApp = initializeApp(config);
