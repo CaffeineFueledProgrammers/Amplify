fe0dc4a2adda6cef2d288ea7542ceb7654439f29
Chris1320 <chris1320is@protonmail.com>
2024-07-22 03:49:12 +0800

    feat: go to home on navbar icon click

--- amplify/src/components/navbar.vue
+++ amplify/src/components/navbar.vue
@@ -1,11 +1,12 @@
 <template>
     <v-app-bar height="90">
-        <img class="navlogo" src="@/assets/logo1@2x.png" alt="logo" width="80" height="80" />
+        <router-link to="/">
+            <img class="navlogo" src="@/assets/logo1@2x.png" alt="logo" width="80" height="80" />
+        </router-link>
         <v-spacer></v-spacer>
         <router-link to="/"><v-btn class="button">Home</v-btn></router-link>
         <router-link to="/login"><v-btn class="button">Log In</v-btn></router-link>
         <v-btn @click="goToSignUp" class="button">Sign Up</v-btn>
-        <router-link :to="{ path: '/dashboard' }"><v-btn class="button">Dashboard</v-btn></router-link>
         <router-link :to="{ path: '/pricing' }"><v-btn class="button">pricing</v-btn></router-link>
     </v-app-bar>
 </template>
@@ -37,6 +38,9 @@ export default {
     methods: {
         goToSignUp() {
             this.$router.push({ path: "/login", query: { section: "signup" } });
+        },
+        goToHome() {
+            this.$router.push({ path: "/" });
         },
     },
 };
